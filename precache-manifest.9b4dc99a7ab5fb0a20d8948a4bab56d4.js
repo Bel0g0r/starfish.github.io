b4d1737a80685ef755fd61905b16d571d1dcbe3e
Bel0g0r <46893238+Bel0g0r@users.noreply.github.com>
2022-02-21 19:03:06 +0300

    StarFish v.1.

---
+++ precache-manifest.9b4dc99a7ab5fb0a20d8948a4bab56d4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27fb66e5249dd0bd50aaf424e1b8c9ba",
+    "url": "/starfish.github.io/index.html"
+  },
+  {
+    "revision": "aa0463e20606e2048472",
+    "url": "/starfish.github.io/static/css/2.2cfa1b23.chunk.css"
+  },
+  {
+    "revision": "bbef8a9d1edb1ba06e5f",
+    "url": "/starfish.github.io/static/css/main.8af72d0e.chunk.css"
+  },
+  {
+    "revision": "aa0463e20606e2048472",
+    "url": "/starfish.github.io/static/js/2.050c40f0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/starfish.github.io/static/js/2.050c40f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbef8a9d1edb1ba06e5f",
+    "url": "/starfish.github.io/static/js/main.c5be08e7.chunk.js"
+  },
+  {
+    "revision": "77ce5f2a0c9a884d2454",
+    "url": "/starfish.github.io/static/js/runtime-main.b6517369.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/starfish.github.io/static/media/persik.4e1ec840.png"
+  }
+]);
